a0a8a0db668ba005fe2c125f2c3e8d6c4e379ce8
Maisson123 <121560037+Maisson123@users.noreply.github.com>
2022-12-28 17:54:18 +0200

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,22 @@
+function clicklinkedin(){
+    window.open("https://www.linkedin.com/in/maison-ahmed-b78a08254/", "_blank");
+}
+function clickgithub(){
+    window.open("https://github.com/Maisson123", "_blank");
+}
+function clicktelegram(){
+    window.open("https://t.me/maison23", "_blank");
+}
+function clickemail(){
+    window.open("", "_blank");
+}
+function clickfacebook(){
+    window.open("https://www.facebook.com/maison.ahmed.3538", "_blank");
+}
+function web(){
+    window.location.href = "front.html";
+}
+function back_end(){
+    window.location.href = 'back_end.html';
+}
+
